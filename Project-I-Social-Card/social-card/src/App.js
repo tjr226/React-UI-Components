3aed74db2e67cd8423700ad976875bf8cef01078
tjr226 <tjr226@gmail.com>
2019-04-22 13:48:13 -0400

    card container implemented
    CardBanner, CardContent, CardContainer all implemented and working in React App

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import './components/HeaderComponents/HeaderContainer.js'
 import HeaderContainer from './components/HeaderComponents/HeaderContainer.js';
+import CardContainer from './components/CardComponents/CardContainer';
 
 
 const App = () => {
   return (
     <div>
       <HeaderContainer />
+      <CardContainer />
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -2,3 +2,8 @@ import React from 'react';
 import './Card.css';
 
 
+const CardBanner = () => {
+    return <img classTitle="card-banner-img" src={'https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png'} alt="atom" />
+};
+
+export default CardBanner
