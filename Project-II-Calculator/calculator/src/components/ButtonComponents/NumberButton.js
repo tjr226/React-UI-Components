909a48d6c9ffef7f30ffc26bcd7f1f1091cb6bf2
tjr226 <tjr226@gmail.com>
2019-04-23 12:51:24 -0400

    first draft implementing NumberButton

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Button.css';
+
+const BasicButton = props => <button className={`basicButtonStyles ${props.buttonStyle}`}>{props.text}</button> ;
+    BasicButton.defaultProps = {
+        buttonStyle: 'teal',
+        text: "Click Me!",
+    };
+
+
+const NumberButton = props => <button className={`${props.buttonStyle}`}>{props.text}</button>;
+    NumberButton.defaultProps = {
+        buttonStyle: 'numberButtonStyle',
+        text: "#",
+    }
